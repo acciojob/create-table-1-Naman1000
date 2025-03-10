ca9f28b91840c4c6a9504e2830f95421e0bb6fbd
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-10 21:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function insert_Row() {
     //Write your code here
-	var table = document.getElementById("myTable");
+	var table = document.getElementById("sampleTable");
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
